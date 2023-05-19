477a0d2228d1a4ba0f011d409186be6388a612fa
lalo0731 <l.eduardo.reyesrodriguez@gmail.com>
2023-05-19 12:57:08 -0700

    Tarjeta footer

--- resources/views/pages/content_index.blade.php
+++ resources/views/pages/content_index.blade.php
@@ -216,9 +216,6 @@
     <div class="row" style="background-color: #c6e4ff; ">
       <div class="col-lg-12">
         <div class="d-flex justify-content-center">
-          <!-- <div class="rayo">
-            <img src="{{asset('images/Rayo.png')}}" alt="Imagen4" class="img-rayo">
-          </div> -->
           <div class="section-heading section-heading__text">
             <h2 class="mb-3 color-text-planes style-text w500">¡Tenemos los mejores planes!</h2>
             <h2><span class="texto-planes style-text w900">¡Obten el que más te guste!</span></h2>
@@ -229,31 +226,25 @@
           <button type="button" class="btn btn-light color-tarje style-text w900 text-capitalize text-capitalize">Prepago</button>
           <button type="button" class="btn btn-light color-tarje style-text w900 text-capitalize text-capitalize">Pago Anticipado</button>
           </div>
-            <!-- <h2 class="mb-3 color-text-planes style-text w500">¡Tenemos los mejores planes!</h2>
-            <h2><span class="texto-planes style-text w900">¡Obten el que más te guste!</span></h2> -->
           </div>
         </div>
       </div>
       
       <hr class="hr-line mt-n2">
       
-      <div class="d-flex justify-content-center">
+      <!-- PLANES #1 -->
+      <div class="d-flex justify-content-center" id="planes_uno">
         <div class="col-lg-3 col-md-6 p-5">
           <div class="pricing-table text-center feature_item font-color box-shad">
             <div class="title">
-              <!-- <h3 class="texto-principal style-text w500">Básico</h3> -->
               <img src="{{asset('images/Paquetes-03.png')}}" alt="Imagen4" class="img-precio1">
             </div>
 
             <div class="price  d-flex flex-row">
                 <div class="col-sm tarjeta-paquete">
-                  <!-- <img src="{{asset('images/280.png')}}" alt="Banner secundario" class="w-100 mx-auto d-block img-basico"> -->
                   <h3 class="texto-principal style-text w500 text-center style-text w900 ">MEGAS</h3>
                   <h3 class="texto-secundario style-text w500 text-center style-text W700">por <span class="style-text w900 ">30 DÍAS</span> </h3>
                 </div>
-                <!-- <div class="col-sm mes-paquete">
-                  <p class="color-priceB animate__animated animate__heartBeat animate__infinite style-text w400">/ Mes</p>
-                </div>  -->
             </div>
 
             <hr class="hr-basico">
@@ -264,35 +255,23 @@
 
             <div class="mt-7">
               <img src="{{asset('images/Paquetes-06.png')}}" alt="Imagen4" class="img-precio1 w-75">
-              <p class="text-uppercase h5 p-2 color-tarjet w600">Incluye compatición de datos*</p>
+              <p class="text-uppercase h5 p-2 color-tarjet w600">Incluye compartición de datos*</p>
             </div>
-
-            <!-- <ul class="feature-list">
-              <li class="style-text w500">1 Mb Subida</li>
-              <li class="style-text w500">5 Mb Bajada</li>
-              <li class="style-text w500">Instalación GRATIS</li>
-              <li class="resticciones style-text w200">* aplican restricciones</li>
-              <hr class="hr-basico">
-            </ul> -->
           </div>
         </div>
 
+        <!-- tARJETA#2 -->
         <div class="col-lg-3 col-md-6 p-5">
           <div class="pricing-table text-center feature_item font-color box-shad">
             <div class="title">
-              <!-- <h3 class="texto-principal style-text w500">Básico</h3> -->
-              <img src="{{asset('images/Paquetes-03.png')}}" alt="Imagen4" class="img-precio1">
+              <img src="{{asset('images/Paquetes-04.png')}}" alt="Imagen4" class="img-precio1">
             </div>
 
             <div class="price  d-flex flex-row">
                 <div class="col-sm tarjeta-paquete">
-                  <!-- <img src="{{asset('images/280.png')}}" alt="Banner secundario" class="w-100 mx-auto d-block img-basico"> -->
                   <h3 class="texto-principal style-text w500 text-center style-text w900 ">MEGAS</h3>
-                  <h3 class="texto-secundario style-text w500 text-center style-text W700">por <span class="style-text w900 ">30 DÍAS</span> </h3>
+                  <h3 class="texto-secundario style-text w500 text-center style-text W700">por <span class="style-text w900 ">10 DÍAS</span> </h3>
                 </div>
-                <!-- <div class="col-sm mes-paquete">
-                  <p class="color-priceB animate__animated animate__heartBeat animate__infinite style-text w400">/ Mes</p>
-                </div>  -->
             </div>
 
             <hr class="hr-basico">
@@ -302,21 +281,42 @@
             </div>
 
             <div class="mt-7">
-              <img src="{{asset('images/Paquetes-06.png')}}" alt="Imagen4" class="img-precio1 w-75">
-              <p class="text-uppercase h5 p-2 color-tarjet w600">Incluye compatición de datos*</p>
+              <img src="{{asset('images/Paquetes-07.png')}}" alt="Imagen4" class="img-precio1 w-75">
+              <p class="text-uppercase h5 p-2 color-tarjet w600">Incluye <span class="color-redes">redes sociales</span></p>
+              <p class="text-uppercase color-tarjet w600 h-1">No permite compartición de datos</p>
             </div>
-
-            <!-- <ul class="feature-list">
-              <li class="style-text w500">1 Mb Subida</li>
-              <li class="style-text w500">5 Mb Bajada</li>
-              <li class="style-text w500">Instalación GRATIS</li>
-              <li class="resticciones style-text w200">* aplican restricciones</li>
-              <hr class="hr-basico">
-            </ul> -->
           </div>
         </div>
 
-        <div class="col-lg-3 col-md-6 p-3">
+        <!-- TARJETA#3 -->
+        <div class="col-lg-3 col-md-6 p-5">
+          <div class="pricing-table text-center feature_item font-color box-shad">
+            <div class="title">
+              <img src="{{asset('images/Paquetes-05.png')}}" alt="Imagen4" class="img-precio1">
+            </div>
+
+            <div class="price  d-flex flex-row">
+                <div class="col-sm tarjeta-paquete">
+                  <h3 class="texto-principal style-text w500 text-center style-text w900 ">MEGAS</h3>
+                  <h3 class="texto-secundario style-text w500 text-center style-text W700">por <span class="style-text w900 ">20 DÍAS</span> </h3>
+                </div>
+            </div>
+
+            <hr class="hr-basico">
+
+            <div class="action-button">
+              <button href="{!! URL::to('recarga')!!}" class="btn btn-main-rounded btn-priceB style-text w800">A TAN SOLO:</button>
+            </div>
+
+            <div class="mt-7">
+              <img src="{{asset('images/Paquetes-08.png')}}" alt="Imagen4" class="img-precio1 w-75">
+              <p class="text-uppercase h5 p-2 color-tarjet w600">Incluye <span class="color-redes">redes sociales</span></p>
+              <p class="text-uppercase color-tarjet w600 h-1">No permite compartición de datos</p>
+            </div>
+          </div>
+        </div>
+
+        <!-- <div class="col-lg-3 col-md-6 p-3">
           <div class="pricing-table featured text-center font-color box-shad">
             <div class="title">
               <h3 class="texto-principal style-text w500">Ideal</h3>
@@ -370,9 +370,230 @@
               <a href="{!! URL::to('recarga')!!}" class="btn btn-main-rounded btn-priceP style-text w800">¡Lo quiero!</a>
             </div>
           </div>
-        </div>
+        </div> -->
 
       </div> 
+
+      <!-- PLANES 2 -->
+      <div class="d-flex justify-content-center" id="planes_dos">
+        <div class="col-lg-3 col-md-6 p-5">
+          <div class="pricing-table text-center feature_item font-color box-shadPlan2">
+            <div class="title">
+              <img src="{{asset('images/Paquetes-19.png')}}" alt="Imagen4" class="img-precio1">
+            </div>
+
+            <div class="price  d-flex flex-row">
+                <div class="col-sm tarjeta-paquete">
+                  <h3 class="texto-principalPlan2 style-text w500 text-center style-text w900 ">MEGAS</h3>
+                  <h3 class="texto-secundarioPlan2 style-text w500 text-center style-text W700">por <span class="style-text w900 ">7 DÍAS</span> </h3>
+                </div>
+            </div>
+
+            <hr class="hr-basico">
+
+            <ul class="feature-list">
+              <li class="style-text w900 w500 texto-secundarioPlan2">5,000 MB</li>
+              <li class="style-text w900 w500 texto-secundarioPlan2">5,000* <span class="h07">MB velocidad reducida</span></li>
+            </ul>
+
+            <div class="action-button">
+              <button href="{!! URL::to('recarga')!!}" class="btn btn-main-rounded btn-priceB2 style-text w800">A TAN SOLO:</button>
+            </div>
+
+            <div class="mt-7">
+              <img src="{{asset('images/Paquetes-24.png')}}" alt="Imagen4" class="img-precio1 w-75">
+              <!-- <p class="text-uppercase h5 p-2 color-tarjet w600">Incluye compartición de datos*</p> -->
+            </div>
+          </div>
+        </div>
+
+        <!-- tARJETA#2 -->
+        <div class="col-lg-3 col-md-6 p-5">
+          <div class="pricing-table text-center feature_item font-color box-shadPlan2">
+            <div class="title">
+              <img src="{{asset('images/Paquetes-20.png')}}" alt="Imagen4" class="img-precio1">
+            </div>
+
+            <div class="price  d-flex flex-row">
+                <div class="col-sm tarjeta-paquete">
+                  <h3 class="texto-principalPlan2 style-text w500 text-center style-text w900 ">MEGAS</h3>
+                  <h3 class="texto-secundarioPlan2 style-text w500 text-center style-text W700">por <span class="style-text w900 ">30 DÍAS</span> </h3>
+                </div>
+            </div>
+
+            <hr class="hr-basico">
+
+            <div class="action-button">
+              <button href="{!! URL::to('recarga')!!}" class="btn btn-main-rounded btn-priceB2 style-text w800">A TAN SOLO:</button>
+            </div>
+
+            <div class="mt-7">
+              <img src="{{asset('images/Paquetes-25.png')}}" alt="Imagen4" class="img-precio1 w-75">
+              <p class="text-uppercase h5 p-2 color-tarjet2 w600">Incluye compartición de datos*</p>
+            </div>
+          </div>
+        </div>
+
+        <!-- TARJETA#3 -->
+        <div class="col-lg-3 col-md-6 p-5">
+          <div class="pricing-table text-center feature_item font-color box-shadPlan2">
+            <div class="title">
+              <img src="{{asset('images/Paquetes-21.png')}}" alt="Imagen4" class="img-precio1">
+            </div>
+
+            <div class="price  d-flex flex-row">
+                <div class="col-sm tarjeta-paquete">
+                  <h3 class="texto-principalPlan2 style-text w500 text-center style-text w900 ">MEGAS</h3>
+                  <h3 class="texto-secundarioPlan2 style-text w500 text-center style-text W700">por <span class="style-text w900 ">30 DÍAS</span> </h3>
+                </div>
+            </div>
+
+            <hr class="hr-basico">
+
+            <ul class="feature-list">
+              <li class="style-text w900 w500 texto-secundarioPlan2">20,000 MB</li>
+              <li class="style-text w900 w500 texto-secundarioPlan2">20,000* <span class="h07">MB velocidad reducida</span></li>
+            </ul>
+
+            <div class="action-button">
+              <button href="{!! URL::to('recarga')!!}" class="btn btn-main-rounded btn-priceB2 style-text w800">A TAN SOLO:</button>
+            </div>
+
+            <div class="mt-7">
+              <img src="{{asset('images/Paquetes-26.png')}}" alt="Imagen4" class="img-precio1 w-75">
+              <p class="text-uppercase h5 p-2 color-tarjet2 w600">No permite compartición de datos*</span></p>
+            </div>
+          </div>
+        </div>
+      </div> 
+
+      <!-- PLANES 3 -->
+      <div class="d-flex justify-content-center" id="planes_tres">
+        <div class="col-lg-3 col-md-6 p-5">
+          <div class="pricing-table text-center feature_item font-color box-shadPlan3" style="position:relative">
+            <span style="width: 100%; height: 5rem; position: absolute; background-color: red; bottom: 0; left: 0;">hola</span>
+            <div class="title">
+              <img src="{{asset('images/Paquetes-09.png')}}" alt="Imagen4" class="img-precio1">
+            </div>
+
+            <div class="price  d-flex flex-row">
+                <div class="col-sm tarjeta-paquete">
+                  <h3 class="texto-principalPlan3 style-text w500 text-center style-text w900 ">MEGAS</h3>
+                  <h3 class="texto-secundarioPlan3 style-text w500 text-center style-text W700">PAQ <span class="style-text w900 ">12 MESES</span> </h3>
+                </div>
+            </div>
+
+            <hr class="hr-basico">
+
+            <div class="action-button">
+              <button href="{!! URL::to('recarga')!!}" class="btn btn-main-rounded btn-priceB3 style-text w800">A TAN SOLO:</button>
+            </div>
+
+            <div class="mt-7">
+              <img src="{{asset('images/Paquetes-13.png')}}" alt="Imagen4" class="img-precio1 w-75">
+              <p class="text-uppercase h5 p-2 color-tarjet3 w600">Incluye compartición de datos*</p>
+            </div>
+          </div>
+        </div>
+
+        <!-- tARJETA#2 -->
+        <div class="col-lg-3 col-md-6 p-5">
+          <div class="pricing-table text-center feature_item font-color box-shadPlan3">
+            <div class="title">
+              <img src="{{asset('images/Paquetes-10.png')}}" alt="Imagen4" class="img-precio1">
+            </div>
+
+            <div class="price  d-flex flex-row">
+                <div class="col-sm tarjeta-paquete">
+                  <h3 class="texto-principalPlan3 style-text w500 text-center style-text w900 ">MEGAS</h3>
+                  <h3 class="texto-secundarioPlan3 style-text w500 text-center style-text W700">PAQ <span class="style-text w900 ">12 MESES</span> </h3>
+                </div>
+            </div>
+
+            <hr class="hr-basico">
+
+            <div class="action-button">
+              <button href="{!! URL::to('recarga')!!}" class="btn btn-main-rounded btn-priceB3 style-text w800">A TAN SOLO:</button>
+            </div>
+
+            <div class="mt-7">
+              <img src="{{asset('images/Paquetes-15.png')}}" alt="Imagen4" class="img-precio1 w-75">
+              <p class="text-uppercase h5 p-2 color-tarjet3 w600">Incluye compartición de datos*</p>
+            </div>
+          </div>
+        </div>
+
+        <!-- TARJETA#3 -->
+        <div class="col-lg-3 col-md-6 p-5">
+          <div class="pricing-table text-center feature_item font-color box-shadPlan3">
+            <div class="title">
+              <img src="{{asset('images/Paquetes-11.png')}}" alt="Imagen4" class="img-precio1">
+            </div>
+
+            <div class="price  d-flex flex-row">
+                <div class="col-sm tarjeta-paquete">
+                  <h3 class="texto-principalPlan3 style-text w500 text-center style-text w900 ">MEGAS</h3>
+                  <h3 class="texto-secundarioPlan3 style-text w500 text-center style-text W700">PAQ <span class="style-text w900 ">12 MESES</span> </h3>
+                </div>
+            </div>
+
+            <hr class="hr-basico">
+
+            <ul class="feature-list">
+              <li class="style-text w900 w500 texto-secundarioPlan3">20,000 MB</li>
+              <li class="style-text w900 w500 texto-secundarioPlan3">20,000* <span class="h07">MB velocidad reducida</span></li>
+            </ul>
+
+            <div class="action-button">
+              <button href="{!! URL::to('recarga')!!}" class="btn btn-main-rounded btn-priceB3 style-text w800">A TAN SOLO:</button>
+            </div>
+
+            <div class="mt-7">
+              <img src="{{asset('images/Paquetes-17.png')}}" alt="Imagen4" class="img-precio1 w-75">
+              <p class="text-uppercase h-1 p-2 color-tarjet3 w600">No permite compartición de datos*</span></p>
+            </div>
+          </div>
+        </div>
+
+        <!-- TARJETA#4 -->
+        <div class="col-lg-3 col-md-6 p-5">
+          <div class="pricing-table text-center feature_item font-color box-shadPlan3">
+            <div class="title">
+              <img src="{{asset('images/Paquetes-11.png')}}" alt="Imagen4" class="img-precio1">
+            </div>
+
+            <div class="price  d-flex flex-row">
+                <div class="col-sm tarjeta-paquete">
+                  <h3 class="texto-principalPlan3 style-text w500 text-center style-text w900 ">MEGAS</h3>
+                  <h3 class="texto-secundarioPlan3 style-text w500 text-center style-text W700">PAQ <span class="style-text w900 ">12 MESES</span> </h3>
+                </div>
+            </div>
+
+            <hr class="hr-basico">
+
+            <ul class="feature-list">
+              <li class="style-text w900 w500 texto-secundarioPlan3">20,000 MB</li>
+              <li class="style-text w900 w500 texto-secundarioPlan3">20,000* <span class="h07">MB velocidad reducida</span></li>
+            </ul>
+
+            <div class="action-button">
+              <button href="{!! URL::to('recarga')!!}" class="btn btn-main-rounded btn-priceB3 style-text w800">A TAN SOLO:</button>
+            </div>
+
+            <div class="mt-7">
+              <img src="{{asset('images/Paquetes-18.png')}}" alt="Imagen4" class="img-precio1 w-75">
+              <p class="text-uppercase h5 p-2 color-tarjet3 w600">Incluye compartición de datos*</p>
+            </div>
+          </div>
+        </div>
+      </div> 
+
+      <hr class="hr-line mt-n2">
+
+      <div class="col-12 p-3">
+        <p class="text-center h5 w500 color-terminos">*Consulta Términos y Condiciones aquí Las velocidades comunicadas son Megas (Mbps) y/o Giga (Gbps) y coresponden a la velocidad máxima de transferencia de datos de bajada ofrecida y alcanzable; la velocidad de bajada y subida puede variar. </p>
+      </div>
+
     <!-- 
       <div class="col-lg-12">
         <div class="owl-testimonials owl-carousel" style="position: relative; z-index: 5;">
@@ -577,5 +798,9 @@
       </div>
     </div>
   </section>
+
+  <script>
+
+  </script>
 
 @endsection
